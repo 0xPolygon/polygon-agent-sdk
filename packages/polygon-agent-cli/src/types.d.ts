432e87165cd2ceae341a8750ce045e3298d63152
Daryl Collins <dcollins@polygon.technology>
2026-03-04 18:25:14 +0000

    chore: dev enhancements add IDEA IDE format-on-save document fix for manually entered `d.ts` files for @0xsequence/dapp-client-cli

--- packages/polygon-agent-cli/src/types.d.ts
+++ packages/polygon-agent-cli/src/types.d.ts
@@ -12,6 +12,7 @@ declare module 'tweetnacl-sealedbox-js' {
   export function overheadLength(): number;
 }
 
+// FIXME: These should be exports from `dapp-client-cli`, but the package has no exports which breaks TS being able to resolve them
 declare module '@0xsequence/dapp-client-cli/dist/state.js' {
   export class StateManager {
     constructor(statePath: string, passphrase: string);
